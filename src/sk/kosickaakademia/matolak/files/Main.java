070784aa542abeabad4910f82acc33a08e1472fd
Adam <adam.matolak@kosickaakademia.sk>
2020-11-11 10:49:44 +0100

    Initial commit

---
+++ src/sk/kosickaakademia/matolak/files/Main.java
@@ -0,0 +1,8 @@
+package sk.kosickaakademia.matolak.files;
+
+public class Main {
+    public static void main(String[] args) {
+        RWFile rwFile= new RWFile();
+        rwFile.copyFileWithCapital("resource/a.txt");
+    }
+}
